6f9c4d1463fb3d9401e2db8f74d6776ce5aa6139
vyaspurva20 <vyaspurva20@gmail.com>
2026-01-26 14:16:40 +0530

    Remove intentional CI failure for testing
    Remove intentional CI failure exception.

--- manage.py
+++ manage.py
@@ -16,7 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-raise Exception("Intentional CI failure to test agent")
+
 
 
 if __name__ == '__main__':
