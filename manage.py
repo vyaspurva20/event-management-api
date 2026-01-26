4e9d1b0cc352bf55f7fff88aa8807d1979308a77
vyaspurva20 <vyaspurva20@gmail.com>
2026-01-26 17:44:44 +0530

    Update DJANGO_SETTINGS_MODULE for configuration

--- manage.py
+++ manage.py
@@ -7,15 +7,15 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault(
         "DJANGO_SETTINGS_MODULE",
-        "event_management_api.settings"  # 👈 change ONLY if your folder name is different
+        "config.settings"   # ✅ THIS IS THE FIX
     )
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "available on your PYTHONPATH environment variable?"
         ) from exc
 
     execute_from_command_line(sys.argv)
