5e816d77a9ae6fe9970e9951986d84327983e9eb
vyaspurva20 <vyaspurva20@gmail.com>
2026-01-26 17:52:25 +0530

    Clean up manage.py and fix indentation
    Removed unnecessary comments and fixed indentation.

--- manage.py
+++ manage.py
@@ -4,11 +4,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE",
-        "config.settings"   # ✅ THIS IS THE FIX
-    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
     try:
         from django.core.management import execute_from_command_line
@@ -17,7 +13,6 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable?"
         ) from exc
-raise Exception("Test CI agent")
 
     execute_from_command_line(sys.argv)
 
